4cc51f788a189b5e946cd57c664a023adac21d7e
JoshB <beitlerjosh@gmail.com>
2025-12-04 23:09:58 -0500

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/MainTeleop.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
-import android.provider.Settings;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
