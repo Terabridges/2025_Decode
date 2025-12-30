fd0bf12aa66fa46c32073c454f727caeed6edaa1
JoshB <beitlerjosh@gmail.com>
2025-12-30 15:53:06 -0500

    Create DistanceSensorTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/DistanceSensorTest.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+
+@TeleOp(name="DistanceSensor", group="Test")
+public class DistanceSensorTest extends LinearOpMode {
+
+    private AnalogInput distanceSensor;
+    private double maxVolts = 3.3;
+    private double maxDistanceMM = 1000;
+
+    @Override
+    public void runOpMode(){
+
+        distanceSensor = hardwareMap.get(AnalogInput.class, "distance_sensor");
+
+
+        waitForStart();
+        while (opModeIsActive()){
+
+            double volts = distanceSensor.getVoltage();
+            double distance = (volts / maxVolts) * maxDistanceMM;
+
+            telemetry.addData("Volts", volts);
+            telemetry.addData("Distance", distance);
+
+        }
+
+    }
+}
