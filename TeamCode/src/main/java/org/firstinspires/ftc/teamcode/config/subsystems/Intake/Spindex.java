5c81313673f62f09262604241998694bed0f24b5
JoshB <beitlerjosh@gmail.com>
2026-02-12 20:28:32 -0500

    b

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Intake/Spindex.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Intake/Spindex.java
@@ -1,15 +1,21 @@
 package org.firstinspires.ftc.teamcode.config.subsystems.Intake;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.util.Timing;
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.config.subsystems.Subsystem;
 import org.firstinspires.ftc.teamcode.config.utility.AbsoluteAnalogEncoder;
+import org.firstinspires.ftc.teamcode.config.utility.Util;
 
+@Configurable
 public class Spindex implements Subsystem {
 
     //---------------- Hardware ----------------
@@ -20,8 +26,43 @@ public class Spindex implements Subsystem {
     private RevColorSensorV3 frontColor;
     private RevColorSensorV3 middleColor;
     private RevColorSensorV3 backColor;
+    Util util;
 
     //---------------- Software ----------------
+    public static double spindexTarget = 0.0;
+    public static double currentPos = 0;
+    private double spindexPower = 0.0;
+
+//    public PIDController spindexPID;
+//    public static double p = 0.0018, i = 0.00005, d = 0.000175; //for larger than one ball
+//    public static double posTolerance = 5;
+//    public static double integrationBounds = 5;
+//    public static double spindexMaxPower = 0.75;
+//    public static double thresh = 0.01;
+//    public static double minPow = 0.08;
+
+    public boolean useSpindexPID = true;
+    private double frontBallOne = 30;
+    private double ballForward = 60;
+    private double switchIntakeForward = 35;
+
+    public static double kPos = 0.012;      // position -> velocity gain (1/sec)
+    public static  double kVel = 0.00035;    // velocity P (power per (ticks/sec))
+    public static double kS   = 0.08;       // static friction feedforward (your minPow idea)
+    public static double maxPower = 0.75;
+
+    public static double aMax = 6000;       // ticks/sec^2  (tune)
+    public static double omegaMax = 9000;   // ticks/sec    (tune)
+    public static double posTol = 5;        // ticks
+    public static double velTol = 50;       // ticks/sec
+
+    // State for velocity estimate
+    private double lastPos = 0.0;
+    private boolean hasLast = false;
+    private double omegaFilt = 0.0;
+    public static double velFilterAlpha = 0.25; // 0..1 (higher = less filtering)
+
+    ElapsedTime timer;
 
 
     //---------------- Constructor ----------------
@@ -35,23 +76,116 @@ public class Spindex implements Subsystem {
         backColor = map.get(RevColorSensorV3.class, "color3");
         spindexAnalog = map.get(AnalogInput.class, "spindexAnalog");
         spindexEnc = new AbsoluteAnalogEncoder(spindexAnalog, 3.3, 0, 1);
+
+//        spindexPID = new PIDController(p, i, d);
+//        spindexPID.setIntegrationBounds(-integrationBounds, integrationBounds);
+//        spindexPID.setTolerance(posTolerance);
+        util = new Util();
+        timer = new ElapsedTime();
     }
 
     //---------------- Methods ----------------
-    public void moveSpindexPow(double pow){
+    public void setSpindexPow(double pow){
         spindexLeft.setPower(pow);
         spindexRight.setPower(pow);
     }
 
+    private static double sign(double x) {
+        return (x > 0) ? 1.0 : (x < 0) ? -1.0 : 0.0;
+    }
+
+//    public double setSpindexPID(double targetPos) {
+//        spindexPID.setPID(p, i, d);
+//        currentPos = spindexEnc.getCurrentPosition();
+//        spindexPower = spindexPID.calculate(currentPos, targetPos);
+//        spindexPower = util.clamp(spindexPower, -spindexMaxPower, spindexMaxPower);
+//        if (spindexPower > thresh){
+//            if (spindexPower < minPow){
+//                spindexPower = minPow;
+//            }
+//        } else if (spindexPower < -thresh){
+//            if (spindexPower > -minPow){
+//                spindexPower = -minPow;
+//            }
+//        }
+//        return spindexPower;
+//    }
+//
+//    public void setSpindex(double target){
+//        setSpindexPow(setSpindexPID(target));
+//    }
+//
+    public double getCurrentPosition(){
+        return currentPos;
+    }
+
+    public double getTargetPosition(){
+        return spindexTarget;
+    }
+
+    public double getSpindexPID(double pos, double target, double dt) {
+        if (dt <= 1e-4) dt = 1e-4;
+
+        // Velocity estimate (ticks/sec) with light filtering to avoid D-noise vibes
+        double omega = 0.0;
+        if (hasLast) omega = (pos - lastPos) / dt;
+        lastPos = pos;
+        hasLast = true;
+        omegaFilt = omegaFilt + velFilterAlpha * (omega - omegaFilt);
+
+        double error = target - pos;
+
+        // Stop condition (prevents tiny oscillation near target)
+        if (Math.abs(error) <= posTol && Math.abs(omegaFilt) <= velTol) {
+            return 0.0;
+        }
+
+        // --- Braking speed limit ---
+        // omegaLimit = sqrt(2 * aMax * |error|)
+        double omegaLimit = Math.sqrt(2.0 * aMax * Math.abs(error));
+        omegaLimit = Math.min(omegaLimit, omegaMax);
+
+        // Position -> desired velocity, then cap it by braking limit
+        double omegaCmd = kPos * error;                 // (ticks/sec)
+        omegaCmd = util.clamp(omegaCmd, -omegaLimit, omegaLimit);
+
+        // Velocity control (simple P) + static friction feedforward
+        double velErr = omegaCmd - omegaFilt;
+        double power = kVel * velErr;
+
+        // Apply kS ONLY when we intend to move (prevents kick-induced oscillation)
+        if (Math.abs(omegaCmd) > 1.0) {
+            power += kS * sign(omegaCmd);
+        }
+
+        // Clamp output
+        power = util.clamp(power, -maxPower, maxPower);
+        spindexPower = power;
+        return spindexPower;
+    }
+
+    public void setSpindex(){
+        setSpindexPow(getSpindexPID(currentPos, spindexTarget, timer.seconds()));
+    }
+
+
     //---------------- Interface Methods ----------------
     @Override
     public void toInit(){
-
+        //spindexTarget = frontBallOne;
+        timer.reset();
     }
 
     @Override
     public void update(){
+        if (useSpindexPID){
+            //setSpindex(spindexTarget);
+            currentPos = spindexEnc.getCurrentPosition();
+            setSpindex();
 
+        } else {
+            setSpindexPow(0);
+        }
+        timer.reset();
     }
-
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Outtake/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Outtake/Shooter.java
@@ -1,31 +1,94 @@
 package org.firstinspires.ftc.teamcode.config.subsystems.Outtake;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.config.subsystems.Subsystem;
+import org.firstinspires.ftc.teamcode.config.utility.Util;
 
+@Configurable
 public class Shooter implements Subsystem {
 
     //---------------- Hardware ----------------
-    private DcMotor leftFlywheel;
-    private DcMotor rightFlywheel;
+    private DcMotorEx leftFlywheel;
+    private DcMotorEx rightFlywheel;
     private Servo hood;
+    private Util util;
 
     //---------------- Software ----------------
+    private final double TICKS_PER_REV = 28.0; // goBILDA 5202/5203
+    private final double SHOOTER_GEAR_RATIO = 1.0;
 
+    public static double hoodDown = 0.0;
+    public static double hoodUp = 1.0;
+
+    public PIDController flywheelPID;
+    public static double p = 0.0, i = 0.0, d = 0.0;
+    public static double posTolerance = 5;
+    public static double integrationBounds = 5;
+    private double flywheelPower = 0.0;
+    public static double flywheelTargetRPM = 0.0;
+    public static double flywheelMaxPower = 1.0;
+    private boolean useFlywheelPID = true;
+    public static double currentRPM = 0;
 
     //---------------- Constructor ----------------
     public Shooter(HardwareMap map) {
-        leftFlywheel = map.get(DcMotor.class, "fly_left");
-        rightFlywheel = map.get(DcMotor.class, "fly_right");
+        leftFlywheel = map.get(DcMotorEx.class, "fly_left");
+        rightFlywheel = map.get(DcMotorEx.class, "fly_right");
         hood = map.get(Servo.class, "hood");
+
+        flywheelPID = new PIDController(p, i, d);
+        flywheelPID.setIntegrationBounds(-integrationBounds, integrationBounds);
+        flywheelPID.setTolerance(posTolerance);
+        util = new Util();
     }
 
     //---------------- Methods ----------------
 
+    public double velToRPM(double tps) {
+        double rps = tps / (TICKS_PER_REV * SHOOTER_GEAR_RATIO);
+        return rps * 60;
+    }
+
+    public double RPMToVel(double rpm) {
+        double tpm = rpm * TICKS_PER_REV * SHOOTER_GEAR_RATIO;
+        return tpm / 60;
+    }
+
+    public void setFlywheelPow(double pow){
+        leftFlywheel.setPower(pow);
+        rightFlywheel.setPower(pow);
+    }
+
+    public double setFlywheelPID(double targetRPM) {
+        flywheelPID.setPID(p, i, d);
+        currentRPM = velToRPM(leftFlywheel.getVelocity());
+        flywheelPower = flywheelPID.calculate(currentRPM, targetRPM);
+        flywheelPower = util.clamp(flywheelPower, -flywheelMaxPower, flywheelMaxPower);
+        return flywheelPower;
+    }
+
+    public void setFlywheel(double target){
+        setFlywheelPow(setFlywheelPID(target));
+    }
+
+    public void setHood(double target){
+        hood.setPosition(target);
+    }
+
+    public double getCurrentRPM(){
+        return currentRPM;
+    }
+
+    public double getTargetRPM(){
+        return flywheelTargetRPM;
+    }
 
     //---------------- Interface Methods ----------------
     @Override
@@ -35,7 +98,11 @@ public class Shooter implements Subsystem {
 
     @Override
     public void update(){
-
+        if (useFlywheelPID){
+            setFlywheel(flywheelTargetRPM);
+        } else {
+            setFlywheelPow(0);
+        }
     }
 
 }
