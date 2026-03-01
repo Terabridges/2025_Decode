ba7559243079041309ab9e722ad2f8c920f777e2
JoshB <beitlerjosh@gmail.com>
2026-03-01 12:53:19 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/BetterTurretTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/BetterTurretTuner.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.tests;
 
 import com.arcrobotics.ftclib.controller.PIDController;
-import com.arcrobotics.ftclib.controller.PIDFController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.ProfiledPIDController;
 import com.arcrobotics.ftclib.controller.wpilibcontroller.SimpleMotorFeedforward;
 import com.arcrobotics.ftclib.trajectory.TrapezoidProfile;
@@ -14,18 +13,15 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.config.subsystems.Robot;
 import org.firstinspires.ftc.teamcode.config.utility.AbsoluteAnalogEncoder;
 import org.firstinspires.ftc.teamcode.config.utility.Util;
 import org.psilynx.psikit.ftc.autolog.PsiKitAutoLog;
 
 @Configurable
 @PsiKitAutoLog(rlogPort = 5802)
-@TeleOp(name="TurretTuner", group="Test")
-public class TurretTuner extends OpMode {
+@TeleOp(name="BetterTurretTuner", group="Test")
+public class BetterTurretTuner extends OpMode {
 
     Gamepad currentGamepad1;
     Gamepad previousGamepad1;
@@ -41,22 +37,20 @@ public class TurretTuner extends OpMode {
 
     private JoinedTelemetry joinedTelemetry;
 
-    public PIDController turretPID;
-    public static double p = 0.01, i = 0.0, d = 0.0005; //feedforward seems to only be adding not subtracting 0.0004
-    public static double posTolerance = 5;
-    public static double integrationBounds = 10;
-    private double turretPower = 0.0;
-    public static double turretTarget = 0.0;
-    public static double turretMaxPower = 1.0;
-    public static double currentPos = 0;
-    private boolean useTurretPID = true;
-
-    public SimpleMotorFeedforward feedforward;
+    private TrapezoidProfile.Constraints constraints;
+    private ProfiledPIDController turretController;
+    private SimpleMotorFeedforward turretFeedforward;
+    public static double maxVel = 180, maxAcc = 900, maxPow = 1.0;
+    public static double p = 0.0, i = 0.0, d = 0.0;
     public static double ks = 0.0, kv = 0.0, ka = 0.0;
 
+    private double minDegrees = 60, maxDegrees = 270;
 
-    private double turretCounterClockwiseLimit = 170;
-    private double turretClowckwiseLimit = 60;
+    public static double turretTarget = 0.0;
+    private double turretPower = 0.0;
+    private double currentPos = 0;
+    private boolean useTurretPID = true;
+
 
     private double TurretForward = 210;
 
@@ -68,35 +62,31 @@ public class TurretTuner extends OpMode {
         turretEnc = new AbsoluteAnalogEncoder(turretAnalog, 3.3, 0, 1);
         leftTurret.setDirection(DcMotorSimple.Direction.REVERSE);
         rightTurret.setDirection(DcMotorSimple.Direction.REVERSE);
-        turretPID = new PIDController(p, i, d);
-        turretPID.setIntegrationBounds(-integrationBounds, integrationBounds);
-        turretPID.setTolerance(posTolerance);
-        feedforward = new SimpleMotorFeedforward(ks, kv, ka);
         util = new Util();
-
         currentGamepad1 = new Gamepad();
         previousGamepad1 = new Gamepad();
-
         currentGamepad2 = new Gamepad();
         previousGamepad2 = new Gamepad();
-
         joinedTelemetry = new JoinedTelemetry(
                 PanelsTelemetry.INSTANCE.getFtcTelemetry(),
                 telemetry
         );
 
+        constraints = new TrapezoidProfile.Constraints(maxVel, maxAcc);
+        turretController = new ProfiledPIDController(p, i, d, constraints);
+        turretFeedforward = new SimpleMotorFeedforward(ks, kv, ka);
     }
 
     @Override
     public void start(){
-
+        turretController.reset(getCurrentPos());
     }
 
     @Override
     public void loop() {
         gamepadUpdate();
         if (useTurretPID){
-            setTurret(turretTarget);
+            //setTurret(turretTarget);
         } else {
             setTurretPow(0);
         }
@@ -120,24 +110,21 @@ public class TurretTuner extends OpMode {
         rightTurret.setPower(pow);
     }
 
-    public double setTurretPID(double targetPos) {
-        turretPID.setPID(p, i, d);
-        currentPos = turretEnc.getCurrentPosition();
-        turretPower = turretPID.calculate(currentPos, targetPos);
-        turretPower = util.clamp(turretPower, -turretMaxPower, turretMaxPower);
-        return turretPower;
-    }
-
-    public void setTurret(double target){
-        setTurretPow(setTurretPID(target));
-    }
-
     public double getCurrentPos(){
-        return currentPos;
+        return turretEnc.getCurrentPosition();
     }
 
     public double getTargetPos(){
         return turretTarget;
     }
+
+//    public double updateTurretController(double currentDeg, double currentVel, double targetDeg, double loopTime) {
+//        targetDeg = util.clamp(targetDeg, minDegrees, maxDegrees);
+//        turretController.setGoal(targetDeg);
+//        double pidOutput = turretController.calculate(currentDeg);
+//
+//        TrapezoidProfile.State sp = turretController.getSetpoint();
+//        double velDes = sp.velocity;
+//    }
 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SpindexTuner.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.config.utility.AbsoluteAnalogEncoder;
+import org.firstinspires.ftc.teamcode.config.utility.Util;
+import org.psilynx.psikit.ftc.autolog.PsiKitAutoLog;
+
+@Configurable
+@PsiKitAutoLog(rlogPort = 5802)
+@TeleOp(name="SpindexTuner", group="Test")
+public class SpindexTuner extends OpMode {
+
+    Gamepad currentGamepad1;
+    Gamepad previousGamepad1;
+
+    Gamepad currentGamepad2;
+    Gamepad previousGamepad2;
+
+    private CRServo spindexLeft;
+    private CRServo spindexRight;
+    private AnalogInput spindexAnalog;
+    private AbsoluteAnalogEncoder spindexEnc;
+    private Util util;
+
+    private JoinedTelemetry joinedTelemetry;
+
+    public PIDFController spindexPID;
+    public static double p = 0.0, i = 0.0, d = 0.0, f = 0.0;
+    public static double posTolerance = 5;
+    public static double integrationBounds = 5;
+    private double spindexPower = 0.0;
+    public static double spindexTarget = 0.0;
+    public static double spindexMaxPower = 1.0;
+    public static double currentPos = 0;
+    private boolean useSpindexPID = true;
+
+    @Override
+    public void init() {
+        spindexLeft = hardwareMap.get(CRServo.class, "spindexL");
+        spindexRight = hardwareMap.get(CRServo.class, "spindexR");
+        spindexAnalog = hardwareMap.get(AnalogInput.class, "spindexAnalog");
+        spindexEnc = new AbsoluteAnalogEncoder(spindexAnalog, 3.3, 29, 1);
+        spindexLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        spindexRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        spindexPID = new PIDFController(p, i, d, f);
+        spindexPID.setIntegrationBounds(-integrationBounds, integrationBounds);
+        spindexPID.setTolerance(posTolerance);
+        util = new Util();
+
+        currentGamepad1 = new Gamepad();
+        previousGamepad1 = new Gamepad();
+
+        currentGamepad2 = new Gamepad();
+        previousGamepad2 = new Gamepad();
+
+        joinedTelemetry = new JoinedTelemetry(
+                PanelsTelemetry.INSTANCE.getFtcTelemetry(),
+                telemetry
+        );
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void loop() {
+        gamepadUpdate();
+        if (useSpindexPID){
+            setSpindex(spindexTarget);
+        } else {
+            setSpindexPow(0);
+        }
+
+        joinedTelemetry.addData("Target Pos", getTargetPos());
+        joinedTelemetry.addData("Current Pos", getCurrentPos());
+        joinedTelemetry.update();
+    }
+
+    public void gamepadUpdate(){
+        previousGamepad1.copy(currentGamepad1);
+        currentGamepad1.copy(gamepad1);
+
+        previousGamepad2.copy(currentGamepad2);
+        currentGamepad2.copy(gamepad2);
+    }
+
+    public void setSpindexPow(double pow){
+        spindexLeft.setPower(pow);
+        spindexRight.setPower(pow);
+    }
+
+    public double setSpindexPID(double targetPos) {
+        spindexPID.setPIDF(p, i, d, f);
+        currentPos = spindexEnc.getCurrentPosition();
+        spindexPower = spindexPID.calculate(currentPos, targetPos);
+        spindexPower = util.clamp(spindexPower, -spindexMaxPower, spindexMaxPower);
+        return spindexPower;
+    }
+
+    public void setSpindex(double target){
+        setSpindexPow(setSpindexPID(target));
+    }
+
+    public double getCurrentPos(){
+        return currentPos;
+    }
+
+    public double getTargetPos(){
+        return spindexTarget;
+    }
+
+
+}
