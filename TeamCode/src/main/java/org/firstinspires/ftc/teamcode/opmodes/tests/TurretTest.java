db7c10de9af73a2e7220aea7852d13a6bc5efa86
JoshB <beitlerjosh@gmail.com>
2026-03-05 21:58:38 -0500

    turret not quite working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/TurretTest.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.config.utility.AbsoluteAnalogEncoder;
+import org.firstinspires.ftc.teamcode.config.utility.Util;
 import org.psilynx.psikit.ftc.autolog.PsiKitAutoLog;
 
 @Configurable
@@ -30,11 +31,13 @@ public class TurretTest extends OpMode {
     public static double leftDegree = 0;
     public static double rightDegree = 0;
     public static double degree = 0;
+    public static double rightOff = 0.031;
 
     private Servo leftTurret;
     private Servo rightTurret;
     private AnalogInput turretAnalog;
     private AbsoluteAnalogEncoder turretEnc;
+    private Util util;
 
     @Override
     public void init() {
@@ -46,6 +49,8 @@ public class TurretTest extends OpMode {
         currentGamepad2 = new Gamepad();
         previousGamepad2 = new Gamepad();
 
+        util = new Util();
+
         joinedTelemetry = new JoinedTelemetry(
                 PanelsTelemetry.INSTANCE.getFtcTelemetry(),
                 telemetry
@@ -55,7 +60,7 @@ public class TurretTest extends OpMode {
         rightTurret = hardwareMap.get(Servo.class, "turretR");
         turretAnalog = hardwareMap.get(AnalogInput.class, "turretAnalog");
         turretEnc = new AbsoluteAnalogEncoder(turretAnalog, 3.3, 0, 1);
-        turretEnc.setInverted(false);
+        turretEnc.setInverted(true);
         leftTurret.setDirection(Servo.Direction.FORWARD);
         rightTurret.setDirection(Servo.Direction.FORWARD);
 
@@ -69,17 +74,20 @@ public class TurretTest extends OpMode {
     @Override
     public void loop() {
         gamepadUpdate();
+        rightDegree = util.clamp(rightDegree/360, 0, 1-rightOff);
+        leftDegree = util.clamp(leftDegree/360, 0, 1-rightOff);
+        degree = util.clamp(degree/360, 0, 1-rightOff);
         if(currentGamepad1.b && !previousGamepad1.b){
-            rightTurret.setPosition(rightDegree/360);
+            rightTurret.setPosition(rightDegree+rightOff);
         }
 
         if(currentGamepad1.x && !previousGamepad1.x){
-            leftTurret.setPosition(leftDegree/360);
+            leftTurret.setPosition(leftDegree);
         }
 
         if(currentGamepad1.y && !previousGamepad1.y){
-            leftTurret.setPosition(degree/360);
-            rightTurret.setPosition(degree/360);
+            leftTurret.setPosition(degree);
+            rightTurret.setPosition(degree+rightOff);
         }
 
         joinedTelemetry.addData("Position", turretEnc.getCurrentPosition());
