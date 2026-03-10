5101b321099ab3c7bdefe187082631183373c959
rb4774 <84111139+rb4774@users.noreply.github.com>
2026-03-10 02:00:18 -0400

    Add PsiKit field auto-log annotations across subsystems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Intake/Clutch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/subsystems/Intake/Clutch.java
@@ -7,8 +7,10 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.config.subsystems.Subsystem;
+import org.psilynx.psikit.ftc.autolog.PsiKitFieldAutoLog;
 
 @Configurable
+@PsiKitFieldAutoLog
 public class Clutch implements Subsystem {
 
     //---------------- Hardware ----------------
